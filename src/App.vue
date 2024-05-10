f2839fcc9991d870742e395ef676cd21e792c1da
kc211 <devadeepkolla211@gmail.com>
2024-05-10 09:25:03 +0530

    got to use the composition api usage

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
-<script>
+<template>
+  <h/>
+  <div class="container">
+    <balance/>
+    <IncomeExpense/>
+    <TransactionList :transactions="transactions"/>
+    <AddTransaction/>
+  </div>
+</template>
+
+<script setup>
 import h from './components/h.vue'
 import Balance from './components/Balance.vue';
 import IncomeExpense from './components/IncomeExpense.vue';
 import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
-export default{
-  components:{
-    h,
-    Balance,
-    IncomeExpense,
-    TransactionList,
-    AddTransaction
-  }
-}
+import {ref} from'vue'
+
+const transactions=ref([
+                {id:1, text:'fiverr', amount:3000},
+                {id:2, text:'camera', amount:-980},
+                {id:3, text:'chegg', amount:500},
+                {id:4, text:'biriyani', amount:-180}
+            ]);
 </script>
-
-
-<template>
-  
-  <h/>
-  <div class="container">
-    <balance/>
-    <IncomeExpense/>
-    <TransactionList/>
-    <AddTransaction/>
-  </div>
-</template>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -5,29 +5,18 @@
             {{ t.text }}<span>Rs {{ t.amount }}</span>
             <button class="delete-btn">x</button>
         </li>
-        <!-- <li class="minus">
-            cash<span>-Rs 400</span>
-            <button class="delete-btn">x</button>
-        </li>
-        <li class="plus">
-            fiverr<span>Rs 800</span>
-            <button class="delete-btn">x</button>
-        </li> -->
+
     </ul>
 </template>
-<script>
+<script setup>
+  
+    import {defineProps} from 'vue';
 
-export default{
-    data(){
-        return {
-            transactions:[
-                {id:1, text:'fiverr', amount:3000},
-                {id:2, text:'camera', amount:-980},
-                {id:3, text:'chegg', amount:500},
-                {id:4, text:'biriyani', amount:-180}
-            ]
+        const props=defineProps({
+        transactions:{
+            type:Array,
+            required:true
         }
-    }
-}
-
+})
+    
 </script>
