432b52eaa5153e0d3393e606410efa641593c94d
kc211 <devadeepkolla211@gmail.com>
2024-05-10 10:12:07 +0530

    the totaling of transactions is done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <h/>
   <div class="container">
-    <balance/>
+    <balance :total="total"/>
     <IncomeExpense/>
     <TransactionList :transactions="transactions"/>
     <AddTransaction/>
@@ -14,12 +14,18 @@ import Balance from './components/Balance.vue';
 import IncomeExpense from './components/IncomeExpense.vue';
 import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
-import {ref} from'vue'
+import {computed, ref} from'vue'
 
 const transactions=ref([
                 {id:1, text:'fiverr', amount:3000},
-                {id:2, text:'camera', amount:-980},
+                {id:2, text:'camera', amount:-980.89},
                 {id:3, text:'chegg', amount:500},
                 {id:4, text:'biriyani', amount:-180}
             ]);
+
+const total = computed( ()=>{
+  return transactions.value.reduce((acc,transaction)=>{
+    return acc+transaction.amount
+  },0);
+})
 </script>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -17,6 +17,7 @@
             type:Array,
             required:true
         }
-})
+});
+
     
 </script>
